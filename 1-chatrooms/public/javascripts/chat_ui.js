2ee54508a614c66819796b3163f3db1be95e7097
rkorsak <rkorsak@gmail.com>
2016-01-17 19:07:34 -0500

    Fix missing or incorrect content from the chapter

--- 1-chatrooms/public/javascripts/chat_ui.js
+++ 1-chatrooms/public/javascripts/chat_ui.js
@@ -1,3 +1,11 @@
+function divEscapedContentElement(message){
+    return $('<div></div>').text(message);
+}
+
+function divSystemContentElement(message){
+    return $('<div></div>').html('<i>' + message + '</i>');
+}
+
 function processUserInput(chatApp, socket) {
     var message = $('#send-message').val();
     var systemMessage;
